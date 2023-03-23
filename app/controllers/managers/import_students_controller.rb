be8da91830eff9522cc2b5b5eb598a1915057095
Antoine Mauriac <mauriac.antoine@gmail.com>
2023-03-23 18:23:29 +0100

    change favicon

--- app/controllers/managers/import_students_controller.rb
+++ app/controllers/managers/import_students_controller.rb
@@ -10,10 +10,11 @@ class Managers::ImportStudentsController < ApplicationController
 
     csv.each do |row|
       row = row.to_hash
+      student = Student.where("lower(unaccent(replace(username, ' ', ''))) = unaccent(?)", row['username'].downcase.gsub(/\s+/, '')).first_or_initialize
 
-      # student = Student.find_or_initialize_by(username: row['username'].downcase)
-      # student = Student.where('lower(username) = ?', row['username'].downcase).first_or_initialize
-      student = Student.where("lower(unaccent(username)) = unaccent(?)", row['username'].downcase.strip).first_or_initialize
+
+      # student = Student.where("lower(unaccent(username)) = unaccent(?)", row['username'].downcase.strip).first_or_initialize
+      # student = Student.where("replace(lower(unaccent(username)), ' ', '') = unaccent(?)", row['username'].downcase.strip).first_or_initialize
 
       student.assign_attributes(student_params_upload(row))
 
