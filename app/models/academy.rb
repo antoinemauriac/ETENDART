fcf19d4db50abad7a8d7e6c1d0fe527064991e4c
Antoine Mauriac <mauriac.antoine@gmail.com>
2023-07-25 12:08:56 +0200

    done

--- app/models/academy.rb
+++ app/models/academy.rb
@@ -23,13 +23,9 @@ class Academy < ApplicationRecord
   end
 
   def today_absent_students
-    students.joins(camps: { courses: :course_enrollments })
-           .joins("INNER JOIN activities ON courses.activity_id = activities.id")
-           .joins("INNER JOIN camps ON activities.camp_id = camps.id")
-           .joins("INNER JOIN school_periods ON camps.school_period_id = school_periods.id")
+    students.joins(courses: :course_enrollments)
            .where(course_enrollments: { present: false })
            .where(courses: { starts_at: Time.current.all_day })
-           .where(school_periods: { academy_id: self.id })
            .distinct
            .order(:last_name)
   end
