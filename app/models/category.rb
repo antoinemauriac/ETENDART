3f36978a966ea2cd9d1824103127a505191dc832
Antoine Mauriac <mauriac.antoine@gmail.com>
2023-11-23 16:54:57 +0100

    fix bug on category absenteism rate

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,7 @@ class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :activities
+  has_many :activity_enrollments, through: :activities
   has_many :courses, through: :activities
   has_many :course_enrollments, through: :courses
   has_many :coach_categories
