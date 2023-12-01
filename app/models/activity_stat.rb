86a1b61a4614588b473de1ff22338c58483aacbd
Antoine Mauriac <mauriac.antoine@gmail.com>
2023-12-01 16:11:07 +0100

    delete comments

--- app/models/activity_stat.rb
+++ app/models/activity_stat.rb
@@ -1,19 +1,3 @@
 class ActivityStat < ApplicationRecord
   belongs_to :activity
 end
-
-# ActivityStat.destroy_all
-
-# school_period = SchoolPeriod.find(14)
-# activities = school_period.activities
-
-# activities.each do |activity|
-#   activity_stat = ActivityStat.new(activity: activity)
-#   activity_stat.students_count = activity.students_count
-#   activity_stat.coaches_count = activity.coaches.count
-#   activity_stat.number_of_boy = activity.number_of_students_by_genre("Garçon")
-#   activity_stat.number_of_girl = activity.number_of_students_by_genre("Fille")
-#   activity_stat.age_of_students = activity.age_of_students
-#   activity_stat.absenteeism_rate = activity.absenteeism_rate
-#   activity_stat.save!
-# end
--- app/models/camp_stat.rb
+++ app/models/camp_stat.rb
@@ -1,39 +1,3 @@
 class CampStat < ApplicationRecord
   belongs_to :camp
 end
-
-
-# CampStat.destroy_all
-# school_period = SchoolPeriod.find(14)
-
-# school_period.camps.each do |camp|
-#   camp_stat = CampStat.new(camp:)
-#   camp_stat.students_count = camp.students_count
-#   camp_stat.coaches_count = camp.coaches.count
-#   camp_stat.show_count = camp.show_count
-#   camp_stat.no_show_count = camp.no_show_count
-#   camp_stat.show_rate = camp.show_rate
-#   camp_stat.no_show_rate = camp.no_show_rate
-#   camp_stat.absenteeism_rate = camp.absenteeism_rate
-
-#   camp_stat.percentage_of_boy = camp.percentage_of_students_by_genre("Garçon")
-#   camp_stat.percentage_of_girl = camp.percentage_of_students_by_genre("Fille")
-
-
-#   camp_stat.age_of_students = camp.age_of_students
-
-#   camp_stat.percentage_of_boy
-#   camp_stat.participant_ages = camp.participant_ages
-
-#   camp_stat.participant_ages.each do |age|
-#     camp_stat.student_count_by_age[age] = camp.number_of_students_by_age(age)
-#   end
-
-#   camp_stat.participant_departments = camp.participant_departments
-
-#   camp_stat.participant_departments.each do |department|
-#     camp_stat.student_count_by_department[department] = camp.number_of_students_by_dpt(department)
-#   end
-
-#   camp_stat.save
-# end
