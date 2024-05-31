ad713d5d8177334197faeef33e5a3d2b64cd7cc4
antoine mauriac <mauriac.antoine@gmail.com>
2024-05-31 10:55:37 +0200

    done

--- app/javascript/controllers/search_coach_controller.js
+++ app/javascript/controllers/search_coach_controller.js
@@ -1,17 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="search-student"
 export default class extends Controller {
 
   static targets = [ "form", "list", "input" ]
 
+  connect() {
+    console.log("hello darling");
+  }
+
   update() {
     const academy_id = this.inputTarget.dataset.academyId
     const query = this.inputTarget.value
-    console.log(academy_id);
-    console.log(query);
+    console.log("hello toto");
     const url = `coaches?academy=${academy_id}&query=${query}`;
-    console.log(url);
     fetch(url, {headers: {"Accept": "text/plain"}})
       .then(response => response.text())
       .then((data) => {
