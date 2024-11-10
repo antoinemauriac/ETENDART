a50ab5c1db745ae66dcec2e4ee04c483132c3a3f
antoine mauriac <mauriac.antoine@gmail.com>
2024-11-10 20:20:01 +0100

    add methode to track non exigible memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -8,10 +8,22 @@ class Membership < ApplicationRecord
   scope :paid, -> { where(status: true) }
   scope :unpaid, -> { where(status: false) }
 
+  # MÉTHODE QUI RENVOIE LES MEMBERSHIPS A PRIORI NON EXIGIBLES
   def self.with_all_course_enrollments_present_false
     joins(student: { course_enrollments: :course })
-      .where('courses.ends_at < ?', Date.current)
-      .group('memberships.id')
+      .where('courses.ends_at BETWEEN ? AND ?', Date.new(2024, 4, 15), Date.current)
+      .group('students.id', 'memberships.id')
       .having('bool_and(course_enrollments.present = false)')
+      .pluck('students.id', 'memberships.id')
+      .to_h
   end
+
+  # SUPPRESSION DES MEMBERSHIPS A PRIORI NON EXIGIBLES SI L'ELEVE N'A PAS DE COURS FUTUR
+  # Membership.with_all_course_enrollments_present_false.each do |student_id, membership_id|
+  #   student = Student.find(student_id)
+  #   membership = Membership.find(membership_id)
+  #   if student.courses.where('starts_at > ?', Date.current).empty?
+  #     membership.destroy
+  #   end
+  # end
 end
