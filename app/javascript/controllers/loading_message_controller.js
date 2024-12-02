4acc3f68a18449cb27c0bc1c40e50e5bc392a1cb
antoine mauriac <mauriac.antoine@gmail.com>
2024-12-02 19:37:34 +0100

    add timeout to show or not the loader

--- app/javascript/controllers/loading_message_controller.js
+++ app/javascript/controllers/loading_message_controller.js
@@ -1,17 +1,34 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="loading-message"
 export default class extends Controller {
-  static targets = [ "turboFrame" ]
+  static targets = [ "turboFrame" ];
+  timeoutId = null;
+
+  connect() {
+    this.turboFrameTarget.addEventListener("turbo:frame-load", () => this.hideLoading());
+  }
 
   showLoading() {
     const turboFrame = this.turboFrameTarget;
-    if (turboFrame) {
-      turboFrame.innerHTML = `
-        <div class="flexy" style="height: 20vh;">
-          <div class="small-loader"></div>
-        </div>
-      `;
+
+    // Délai avant d'afficher le loader (1 seconde ici)
+    this.timeoutId = setTimeout(() => {
+      if (turboFrame) {
+        turboFrame.innerHTML = `
+          <div class="flexy" style="height: 20vh;">
+            <div class="small-loader"></div>
+          </div>
+        `;
+      }
+    }, 800);
+  }
+
+  hideLoading() {
+    // Annuler le timeout si le chargement est terminé avant 1 seconde
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+      this.timeoutId = null;
     }
   }
 }
