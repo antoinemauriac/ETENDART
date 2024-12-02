8d04d068c6ccf1e98c60756c50db4fdb828be443
antoine mauriac <mauriac.antoine@gmail.com>
2024-12-02 20:15:40 +0100

    done

--- app/javascript/controllers/loading_message_controller.js
+++ app/javascript/controllers/loading_message_controller.js
@@ -1,35 +1,29 @@
-import { Controller } from "@hotwired/stimulus";
-
 // Connects to data-controller="loading-message"
 export default class extends Controller {
-  static targets = ["turboFrame"];
+  static targets = [ "turboFrame" ];
   timeoutId = null;
 
   connect() {
-    if (this.hasTurboFrameTarget) {
-      this.turboFrameTarget.addEventListener("turbo:frame-load", this.hideLoading.bind(this));
-    }
-  }
-
-  disconnect() {
-    if (this.hasTurboFrameTarget) {
-      this.turboFrameTarget.removeEventListener("turbo:frame-load", this.hideLoading.bind(this));
-    }
+    this.turboFrameTarget.addEventListener("turbo:frame-load", () => this.hideLoading());
   }
 
   showLoading() {
-    if (this.hasTurboFrameTarget) {
-      this.timeoutId = setTimeout(() => {
-        this.turboFrameTarget.innerHTML = `
+    const turboFrame = this.turboFrameTarget;
+
+    // Délai avant d'afficher le loader (1 seconde ici)
+    this.timeoutId = setTimeout(() => {
+      if (turboFrame) {
+        turboFrame.innerHTML = `
           <div class="flexy" style="height: 20vh;">
             <div class="small-loader"></div>
           </div>
         `;
-      }, 500);
-    }
+      }
+    }, 800);
   }
 
   hideLoading() {
+    // Annuler le timeout si le chargement est terminé avant 1 seconde
     if (this.timeoutId) {
       clearTimeout(this.timeoutId);
       this.timeoutId = null;
