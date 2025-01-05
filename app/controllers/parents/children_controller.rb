4c1dddf334160254f4cd7be0d9ccd96c22a3a466
Adam Lamouri <adamlamouri@1344.fr>
2025-01-05 01:25:27 +0100

    affichage des infos de student en tant que child du parent

--- app/controllers/parents/children_controller.rb
+++ app/controllers/parents/children_controller.rb
@@ -14,34 +14,55 @@ class Parents::ChildrenController < ApplicationController
   def create
     @child = Student.new(child_params)
     authorize [:parents, :student], :create?
+
+    # Calcul du username basé sur la date de naissance
     excel_serial_date = (@child.date_of_birth - Date.new(1899, 12, 30)).to_i
-		username = "#{@child.first_name.downcase}#{@child.last_name.downcase}#{excel_serial_date }"
+    username = "#{@child.first_name.downcase}#{@child.last_name.downcase}#{excel_serial_date}"
 
-		# je check si ce username existe déjà dans la base
-		existing_child = Student.find_by(username: username)
+    # Vérifie si un étudiant avec ce username existe déjà
+    existing_child = Student.find_by(username: username)
 
-    # si il existe déjà, je ne crée pas de nouvelle instance de child
-    # mais je mets à jour les infos rentrées par le parent concernant addresse, allergies, email...
     if existing_child
       @child = existing_child
-      @child.update(child_params)
 
-    # sinon je crée un nouveau child
+      # Met à jour uniquement les champs qui ne sont pas déjà définis
+      child_params.each do |key, value|
+        @child[key] = value if @child.respond_to?(key) && @child[key].blank?
+      end
+
+      # Définir un message spécifique pour un enfant retrouvé et mis à jour
+      notice_message = "L'enfant a bien été retrouvé et mis à jour."
     else
       @child.username = username
+
+      # Définir un message spécifique pour un nouvel enfant créé
+      notice_message = "L'élève a bien été créé."
     end
 
     @child.parent = @parent
     if @child.save
-      redirect_to parents_child_path(@child), notice: 'L\'élève a bien été créé.'
+      redirect_to parents_child_path(@child), notice: notice_message
     else
-      redirect_to new_parents_child_path, alert: 'Une erreur est survenue lors de la création de l\'élève.'
+      render :new, alert: 'Une erreur est survenue lors de la création de l\'élève.'
     end
   end
 
   def show
     @child = Student.find(params[:id])
     authorize [:parents, :student], :show?
+    @academies = @child.academies.uniq
+    @academy1 = @child.academies.first if @child.academies.any?
+    @academy2 = @child.academies.second if @child.academies.second
+    @academy3 = @child.academies.third if @child.academies.third
+    @academy = @child.first_academy
+    @feedback = Feedback.new
+    @feedbacks = @child.feedbacks.order(created_at: :desc)
+    @start_year = Date.current.month >= 9 ? Date.current.year : Date.current.year - 1
+    @membership = @child.memberships.find_by(start_year: @start_year)
+    @memberships = @child.memberships.includes(:receiver).order(start_year: :desc)
+    @camp_enrollments = @child.paid_camp_enrollments_without_free_judo_activity.includes(:camp, :receiver)
+    @camp_enrollment = @camp_enrollments.last
+
   end
 
   def edit
@@ -67,7 +88,7 @@ class Parents::ChildrenController < ApplicationController
   end
 
   def child_params
-    params.require(:child).permit(:first_name, :last_name, :date_of_birth, :gender, :photo, :siblings_count, :email, :phone_number, :school, :has_medical_treatment, :medical_treatment_description, :has_consent_for_photos, :rules_signed)
+    params.require(:student).permit(:first_name, :last_name, :date_of_birth, :gender, :photo, :siblings_count, :email, :phone_number, :school, :has_medical_treatment, :medical_treatment_description, :has_consent_for_photos, :rules_signed)
   end
 
 end
