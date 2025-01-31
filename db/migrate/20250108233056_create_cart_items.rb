bd8d5c7e5478d12c598e075ef4c110a134afbbb5
Adam Lamouri <adamlamouri@1344.fr>
2025-01-31 10:42:19 +0100

    sauvegarde de la bonne migration

--- db/migrate/20250108233056_create_cart_items.rb
+++ db/migrate/20250108233056_create_cart_items.rb
@@ -1,7 +1,7 @@
 class CreateCartItems < ActiveRecord::Migration[7.0]
   def change
     create_table :cart_items do |t|
-      t.string :product
+      # t.string :products
       t.decimal :price
 
       t.references :cart, null: false, foreign_key: true
--- db/migrate/20250113040941_rename_products_columns_in_cart_items.rb
+++
@@ -1,6 +0,0 @@
-class RenameProductsColumnsInCartItems < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :cart_items, :products_type, :product_type
-    rename_column :cart_items, :products_id, :product_id
-  end
-end
