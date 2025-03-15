c6a6ceb701d2ba94f88b41b14c1059ed51b23408
Adam Lamouri <adamlamouri@1344.fr>
2025-03-15 05:06:07 +0100

    Correction des méthodes de paiement en capitalisant "Carte bancaire" et amélioration de l'affichage des éléments du panier avec une nouvelle mise en page.

--- app/javascript/controllers/home/filter_controller.js
+++ app/javascript/controllers/home/filter_controller.js
@@ -3,13 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="home--filter"
 export default class extends Controller {
 
-  static targets = [ "academy" ]
+  static targets = [ "academy", "button" ]
   connect() {
     console.log("ceci est pour la homepage");
   }
 
   filter(event) {
-    console.log("ca filtre");
+    this.buttonTargets.forEach(button => {
+      if (button === event.currentTarget) {
+        button.classList.add("bg-white");
+        button.classList.remove("bg-secondary");
+      } else {
+        button.classList.add("bg-secondary");
+        button.classList.remove("bg-white");
+      }
+    });
+
 
     const selectedAcademy = event.target.dataset.academyId;
 
