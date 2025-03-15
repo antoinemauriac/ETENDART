ee0b79b8b94ee417742381ca8f74ea991c9e6af0
antoine mauriac <mauriac.antoine@gmail.com>
2025-03-15 22:13:45 +0100

    done

--- app/javascript/controllers/home/filter_controller.js
+++ app/javascript/controllers/home/filter_controller.js
@@ -3,12 +3,26 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="home--filter"
 export default class extends Controller {
 
-  static targets = [ "academy", "button" ]
+  static targets = [ "academy", "button" , "allButton"]
   connect() {
-    console.log("ceci est pour la homepage");
+    console.log("Connected to home--filter controller")
+    this.buttonTargets.forEach(button => {
+      button.classList.add("bg-secondary");
+      button.classList.remove("bg-white");
+    });
+    this.allButtonTarget.classList.add("bg-white");
+    this.allButtonTarget.classList.remove("bg-secondary");
   }
 
   filter(event) {
+    if (event.target === this.allButtonTarget) {
+      this.allButtonTarget.classList.add("bg-white");
+      this.allButtonTarget.classList.remove("bg-secondary");
+    } else {
+      this.allButtonTarget.classList.add("bg-secondary");
+      this.allButtonTarget.classList.remove("bg-white");
+    }
+
     this.buttonTargets.forEach(button => {
       if (button === event.currentTarget) {
         button.classList.add("bg-white");
