17da8e3b3f15fb213332828f553f175fa46b03eb
antoine mauriac <mauriac.antoine@gmail.com>
2025-03-20 15:59:00 +0100

    Refactor views and styles for improved consistency and readability; remove unused components and update class names for better clarity

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ require 'sidekiq/cron/web'
 Rails.application.routes.draw do
 
   # Pas besoin d'être authentifié pour accéder aux pages suivantes
-  resources :academies, only: %i[index show] do
-    resources :school_periods, only: %i[show] do
-      resources :activities, only: %i[show] do
-        resources :activity_enrollments, only: %i[create]
-      end
-    end
-  end
-  resources :activity_enrollments, only: %i[destroy]
+  # resources :academies, only: %i[index show] do
+  #   resources :school_periods, only: %i[show] do
+  #     resources :activities, only: %i[show] do
+  #       resources :activity_enrollments, only: %i[create]
+  #     end
+  #   end
+  # end
+  # resources :activity_enrollments, only: %i[destroy]
 
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
