3079119856c4abf4372dbd1947b9fc25177cd83c
Adam Lamouri <adamlamouri@1344.fr>
2025-03-26 15:58:48 +0100

    template email inscription

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
